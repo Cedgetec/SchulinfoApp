22c3490e65efc33810c3bc4412cf7056a8da0476
Gebatzens <gebatzens@gebatzens.de>
2015-03-31 18:59:14 +0200

    Fix license

--- app/src/main/java/de/gebatzens/ggvertretungsplan/view/GGSwipeLayout.java
+++ app/src/main/java/de/gebatzens/ggvertretungsplan/view/GGSwipeLayout.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Fabian Schultis, Hauke Oldsen
+ * Copyright 2015 Hauke Oldsen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
