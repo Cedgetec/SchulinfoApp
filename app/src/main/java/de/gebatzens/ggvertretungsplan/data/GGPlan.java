384784fb6ffbc3cb1c08f3196b0811f61ca57633
asterix11 <fabian@microbotik.de>
2015-05-31 20:51:40 +0200

    Changed DATE field in Databae.

--- app/src/main/java/de/gebatzens/ggvertretungsplan/data/GGPlan.java
+++ app/src/main/java/de/gebatzens/ggvertretungsplan/data/GGPlan.java
@@ -352,7 +352,7 @@ public class GGPlan {
     public static Date parseDate(String date) {
         if(date.length() < 8)
             date += new SimpleDateFormat("yyyy").format(new Date());
-        SimpleDateFormat fmt = new SimpleDateFormat("d.M.yyyy");
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
         try {
             return fmt.parse(date);
         } catch(Exception e) {
