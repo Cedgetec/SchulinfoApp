ee005af90f3c054de7cb11cfac0fcd23450b113b
LRAnimations <lranimations@gmail.com>
2016-01-28 11:20:41 +0100

    Fix special message width

--- app/src/main/java/de/gebatzens/sia/fragment/SubstPagerFragment.java
+++ app/src/main/java/de/gebatzens/sia/fragment/SubstPagerFragment.java
@@ -127,6 +127,7 @@ public class SubstPagerFragment extends RemoteDataFragment {
 
     public SubstListAdapter.MessageViewHolder createSMCard(ViewGroup parent, LayoutInflater inflater) {
         FrameLayout f2 = new FrameLayout(getActivity());
+        f2.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         f2.setPadding(toPixels(1.3f), toPixels(0.3f), toPixels(1.3f), toPixels(0.3f));
         CardView cv = (CardView) inflater.inflate(R.layout.basic_cardview, f2, false);
         cv.setId(R.id.cvroot);
