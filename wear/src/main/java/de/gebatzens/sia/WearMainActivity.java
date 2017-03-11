6f71968f1d02671516c5aded6458434b1d58f21f
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2017-03-11 02:12:07 +0100

    Improve android wear support, new apis etc.

--- wear/src/main/java/de/gebatzens/sia/WearMainActivity.java
+++ wear/src/main/java/de/gebatzens/sia/WearMainActivity.java
@@ -17,7 +17,6 @@ package de.gebatzens.sia;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
 public class WearMainActivity extends Activity {
@@ -28,14 +27,8 @@ public class WearMainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                test = (TextView) findViewById(R.id.text);
-            }
-        });
 
+        test = (TextView) findViewById(R.id.text);
     }
 
     @Override
